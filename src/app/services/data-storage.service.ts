f1668c6748a4c07d0af04839c9754b868b66ff67
Bogdan Lipuš <bolipus@gmail.com>
2020-03-01 22:02:19 +0100

    Small update

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,25 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { RecipeService } from './recipe.service';
-import { Recipe } from '../recipes/recipe';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { Recipe } from '../recipes/recipe';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     console.log('storeRecipe');
     const token = this.authService.getToken();
     this.http
       .put<{ name: string }>(
-        'https://test-http-cefa0.firebaseio.com/recipes.json?auth='+token,
+        'https://test-http-cefa0.firebaseio.com/recipes.json',
         this.recipeService.getRecipes(),
         {
-          observe: 'response'
+          observe: 'response',
+          params: new HttpParams().append('auth', token),
+         reportProgress: true
         }
       )
       .subscribe(
@@ -35,10 +41,16 @@ export class DataStorageService {
   fetchRecipes() {
     const token = this.authService.getToken();
     this.http
-      .get<Recipe[]>('https://test-http-cefa0.firebaseio.com/recipes.json?auth=' + token, {
-        headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
-        responseType: 'json'
-      })
+      .get<Recipe[]>(
+        'https://test-http-cefa0.firebaseio.com/recipes.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          responseType: 'json',
+          params: new HttpParams().append('auth', token),
+          reportProgress: true
+
+        }
+      )
       .pipe(
         map((recipes: Recipe[]) => {
           for (const recipe of recipes) {
@@ -49,10 +61,10 @@ export class DataStorageService {
           }
           return recipes;
         })
-      ).subscribe( (recipes: Recipe[]) => {
+      )
+      .subscribe((recipes: Recipe[]) => {
         console.log(recipes);
         this.recipeService.setRecipes(recipes);
-      }
-    );
+      });
   }
 }
